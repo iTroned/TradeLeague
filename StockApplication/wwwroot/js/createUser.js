f8c3921b7be4ddb81a70ccfffc453ca5a5243a75
go4sw <s351896@oslomet.no>
2022-10-10 15:38:24 +0200

    Navbar

--- StockApplication/wwwroot/js/createUser.js
+++ StockApplication/wwwroot/js/createUser.js
@@ -5,7 +5,7 @@
     const url = "Stock/createUser";
     $.post(url, user, function (OK) {
         if (OK) {
-            window.location.href = 'index.html';
+            window.location.href = 'home.html';
         }
         else {
             $("#error").html("Something went wrong");
