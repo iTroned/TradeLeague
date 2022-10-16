2ac848c4318000110abc1200945c42becf072ed1
luddebassen <luddebassen213@gmail.com>
2022-10-16 21:03:08 +0200

    Charts for company

--- StockApplication/wwwroot/js/chartCompany.js
+++ StockApplication/wwwroot/js/chartCompany.js
@@ -6,15 +6,29 @@
     $.get(url, function (company) {
         $("#name").html(company.name);
         
-        chart(company.values);
+        chart(company);
         formatCompany(company);
     });
 
 });
 
-function chart(values) {
-    
-    $("#chart").html(values);
+function chart(company) {
+    const array = JSON.parse(company.values);
+    const ctx = document.getElementById('companyChart').getContext('2d');
+    const labels = ["10 mins ago", "9 mins ago", "8 mins ago", "7 mins ago", "6 mins ago", "5 mins ago", "4 mins ago", "3 mins ago", "2 mins ago", "1 min ago"];
+    const companyChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: company.name,
+                data: array,
+                fill: false,
+                borderColor: 'rgb(75,192,192)',
+                tension: 0.1
+            }]
+        }
+    })
 }
 
 function formatCompany(company) {
