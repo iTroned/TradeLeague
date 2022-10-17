3af05beb5ad6d988792ed81caed3a02f0b54205a
luddebassen <luddebassen213@gmail.com>
2022-10-17 15:15:04 +0200

    new functions

--- StockApplication/Code/Handlers/IStockRepository.cs
+++ StockApplication/Code/Handlers/IStockRepository.cs
@@ -17,5 +17,23 @@ namespace StockApplication.Code.Handlers
         Task<List<Company>> getAllCompanies();
         Task<bool> deleteUser(string id);
         Task<bool> deleteCompany(string id);
+        Task<bool> tryToBuyStockForUser(string userID, string companyID, int amount);
+        Task<bool> tryToSellStockForUser(string userID, string companyID, int amount);
+        Task<float> getBalanceForUser(Guid id);
+        Task<bool> setBalanceForUser(User user, float balance);
+        Task<Stock> getStockByID(Guid id);
+        Task<bool> deleteStock(Guid id);
+        Task<bool> addBalanceToUser(User user, float value);
+        Task<bool> removeBalanceFromUser(User user, float value);
+        Task<bool> userHasEnoughBalance(User user, float value);
+        Task<bool> addStockToUser(User user, Company company, int amount);
+        Task<bool> createStock(User user, Company company, int amount);
+        Task<bool> removeStockFromUser(User user, Company company, int amount);
+        Task<List<Stock>> getStocksWithUser(User user);
+        Task<List<Stock>> getStocksWithCompany(Company company);
+        Task<Stock> getStockWithUserAndCompany(User user, Company company);
+        Task<bool> userHasStocksWithCompany(User user, Company company);
+        Task<bool> updateValues();
+
     }
 }
